884a6ce7c74eeac0c9b189e845a556c50121aeb3
Warislone <107351727+Warislone@users.noreply.github.com>
2024-09-05 15:09:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7463949353:AAHR9D4oFTrbISl8xQuZmBZNUJ10KzrODcM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("THEBOYZZX")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1121148774")
 
 
 
